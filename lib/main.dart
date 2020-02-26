c88f765cff7f47dc6873df30f94589cbecb78364
Heitor Almeida <heitor.haga@gmail.com>
2020-02-26 20:29:26 -0300

    Testing crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
-import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(FlashChat());
+void main() {
+  // Set `enableInDevMode` to true to see reports while in debug mode
+  // This is only to be used for confirming that reports are being
+  // submitted as expected. It is not intended to be used for everyday
+  // development.
+  Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
